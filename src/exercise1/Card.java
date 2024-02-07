fed6e646ab437bebc83848734fa936949553333e
Felix Kunjachan Lukose <kunjacha@sheridancollege.ca>
2024-02-07 13:39:08 -0500

    commit

--- src/exercise1/Card.java
+++ src/exercise1/Card.java
@@ -16,6 +16,14 @@ public class Card {
    private int value;//1-13
 
    public static final String [] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
+
+    Card(int value, String suit) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    Card() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     /**
      * @return the suit
      */
--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -1,67 +1,77 @@
 package exercise1;
 
+import java.util.Scanner;
+
 /**
  * A class that fills a hand of 7 cards with random Card Objects and then asks the user to pick a card.
- * It then searches the array of cards for the match to the user's card. 
+ * It then searches the array of cards for the match to the user's card.
  * To be used as starting code in Exercise
  *
- * @author dancye
- * @author Paul Bonenfant Jan 25, 2022 
+ * @author Your Name
+ * @date February 7, 2024
  */
 public class CardTrick {
-    
+
     public static void main(String[] args) {
-        
         Card[] hand = new Card[7];
 
+        // Fill the hand with random Card objects
         for (int i = 0; i < hand.length; i++) {
             Card card = new Card();
-            //card.setValue(insert call to random number generator here)
-            // 
-            //card.setSuit(Card.SUITS[insert call to random number between 0-3 here])
-            // Hint: You can use Random -> random.nextInt(n) to get a random number between 0 and n-1 (inclusive)
-            //       Don't worry about duplicates at this point
+            card.setValue(generateRandomValue());
+            card.setSuit(Card.SUITS[generateRandomSuit()]);
+            hand[i] = card;
         }
 
-        // insert code to ask the user for Card value and suit, create their card
-        // and search the hand here. 
-        // Hint: You can ask for values 1 to 10, and then
-        //       11 for jack, 12 for queen, etc. (remember arrays are 0-based though)
-        //       1 for Hearts, 2 for Diamonds, etc. (remember arrays are 0-based though)
-        // 
-        // Then loop through the cards in the array to see if there's a match.
-        
-        // If the guess is successful, invoke the printInfo() method below.
-        
+        // Display the generated cards (for debugging purposes)
+        System.out.println("Generated Cards:");
+        for (Card card : hand) {
+            System.out.println(card);
+        }
+
+        // Ask the user for a Card value and suit
+        Card userCard = getUserCard();
+
+        // Search the hand for the user's card
+        boolean found = false;
+        for (Card card : hand) {
+            if (card.equals(userCard)) {
+                found = true;
+                break;
+            }
+        }
+
+        // If the user's card is in the hand, invoke the printInfo() method
+        if (found) {
+            printInfo();
+        } else {
+            System.out.println("Sorry, your card is not in the hand.");
+        }
     }
 
-    /**
-     * A simple method to print out personal information. Follow the instructions to 
-     * replace this information with your own.
-     * @author Paul Bonenfant Jan 2022
-     */
+    private static Card getUserCard() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the value of your card (1-13): ");
+        int value = scanner.nextInt();
+
+        System.out.print("Enter the suit of your card (Hearts, Diamonds, Clubs, Spades): ");
+        String suit = scanner.next();
+
+        return new Card(value, suit);
+    }
+
+    // Rest of the code (generateRandomValue, generateRandomSuit, printInfo) remains unchanged.
+
+    private static int generateRandomSuit() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    private static int generateRandomValue() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
     private static void printInfo() {
-    
-        System.out.println("Congratulations, you guessed right!");
-        System.out.println();
-        
-        System.out.println("My name is Paul, but you can call me prof, Paul or sir");
-        System.out.println();
-        
-        System.out.println("My career ambitions:");
-        System.out.println("-- Be more active on LinkedIn");
-        System.out.println("-- Have a semester with no violations of academic integrity!");
-	System.out.println();	
-
-        System.out.println("My hobbies:");
-        System.out.println("-- Investing");
-        System.out.println("-- Cooking");
-        System.out.println("-- Reading/Watching TV");
-        System.out.println("-- Riding my motorcycle");
-
-        System.out.println();
-        
-    
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-
 }
